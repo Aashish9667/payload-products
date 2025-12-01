2a2bac708425c59af28c2cca29351e72342ae8a0
code with harry <aashish.kumar@96672gmail.com>
2025-12-01 11:37:49 +0530

    feat: error resolve

---
+++ src/app/(frontend)/products/page.tsx
@@ -0,0 +1,30 @@
+export default async function ProductsPage() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+
+  const res = await fetch(`${baseUrl}/api/products`, {
+    next: { revalidate: 5 },
+  });
+  if (!res.ok) {
+    throw new Error(`Failed to fetch products: ${res.status}`);
+  }
+
+  const data = await res.json();
+
+  return (
+    <div>
+      {data.docs.map((product: any) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>₹{product.price}</p>
+          <img
+            src={product.image?.url}
+            alt={product.title}
+            width={200}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
